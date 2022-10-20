c7e71b7fe20ae98c738c500bf0209eb01bf4fb6b
Aino <n1huai00@students.oamk.fim>
2022-10-20 20:10:39 +0300

    koodi

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 
 $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
-$sql="select * from task";
+$sql="select * from item";
 $query = $db->query($sql);
 $results= $query->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($results);
